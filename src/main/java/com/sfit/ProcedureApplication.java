080b0d53c5dc73d62ead6db068ac0abd57c1db0a
费昌勇 <fei.changyong@sfit.shfe.com.cn>
2018-11-01 19:37:41 +0800

    init procedure

---
+++ src/main/java/com/sfit/ProcedureApplication.java
@@ -0,0 +1,12 @@
+package com.sfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcedureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcedureApplication.class, args);
+	}
+}
